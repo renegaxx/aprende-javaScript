1139cbcc0d5c392882863b47afee7131494c27c4
renegaxx <133661895+renegaxx@users.noreply.github.com>
2023-08-01 16:44:48 -0300

    Initial commit

---
+++ javao.js
@@ -0,0 +1,11 @@
+
+       function img1(){
+            
+       let Nome = prompt("Informe o nome da figura selecionada");
+       alert("Boa, a Figura é um "+Nome);
+       let x= Nome=Number(prompt("Informe a letra da direita"));
+       let y= Nome=Number(prompt("Informe a letra de baixo"));
+       let resultado=x*y;
+       alert("O resultado da soma é "+resultado);
+        }
+       
